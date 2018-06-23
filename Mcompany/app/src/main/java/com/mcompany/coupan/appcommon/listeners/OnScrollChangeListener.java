8a71ba46ce3296876d51a380e0bd16c806bcaed2
MANMOHAN4154-01\Nagarro <mohanman2210@gmail.com>
2018-06-23 16:06:46 +0530

    Code refinement

--- Mcompany/app/src/main/java/com/mcompany/coupan/appcommon/listeners/OnScrollChangeListener.java
+++
@@ -1,9 +0,0 @@
-package com.mcompany.coupan.appcommon.listeners;
-
-/**
- * Created by manmohansingh on 20-02-2018.
- */
-
-public interface OnScrollChangeListener {
-    void onScrollChange(String scrollDirection, boolean isScrolling);
-}
